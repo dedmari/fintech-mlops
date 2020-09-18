121df28e4a6de84db8aa6b230e5336dbcd7c4d10
muneer7589 <muneer7589@gmail.com>
2020-09-18 16:46:37 +0200

    included running different pipeline versions

--- kfp-pipeline/kfp_run_pipeline.py
+++ kfp-pipeline/kfp_run_pipeline.py
@@ -16,11 +16,17 @@ with open('./config/pipeline.json') as pipeline_config:
 experiment_id = pipeline_metadata['experiment_id']
 pipeline_id = pipeline_metadata['pipeline_id']
 run_name = pipeline_metadata['pipeline_run_name']
+pipeline_version_id = pipeline_metadata['pipeline_version_id']
 
 client = kfp.Client()
 
-run = client.run_pipeline(experiment_id=experiment_id, pipeline_id=pipeline_id, job_name=run_name,
-                          params=pipeline_run_params)
+if pipeline_metadata['use_existing_pipeline'] == "True":
+    run = client.run_pipeline(experiment_id=experiment_id, pipeline_id=pipeline_id, version_id=pipeline_version_id,
+                              job_name=run_name,
+                              params=pipeline_run_params)
+else:
+    run = client.run_pipeline(experiment_id=experiment_id, pipeline_id=pipeline_id, job_name=run_name,
+                              params=pipeline_run_params)
 print('Run link: %s%s/#/runs/details/%s' % (kubeflow_host, client._get_url_prefix(), run.id))
 
 with open('recent_run_id.txt', 'w') as run_id:
